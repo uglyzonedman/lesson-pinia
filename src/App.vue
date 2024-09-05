e015a2e52516436707ba4d49f35fa0827dead668
unknown <MoiseenkoD@lcoast.itstep>
2024-09-05 20:04:21 +0400

    Создание проекта

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TodoList from './components/TodoList.vue';
+
+</script>
+
+<template>
+  <TodoList></TodoList>
+</template>
+
+<style scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>Todo List</h1>
+        <input type="text" v-model="text" @keyup.enter="addTodo" placeholder="Add a new todo">
+
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                <span :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }">
+                    {{ todo.text }}</span>
+                <button @click="removeTodo(todo.id)">Remove</button>
+                <button @click="toggleTodo(todo.id)">Toggle</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup >
+import { ref } from 'vue'
+import { useTodoStore } from '../stores/todoStore'
+
+const todoStore = useTodoStore()
+
+const text = ref('')
+
+const addTodo = () => {
+    if (text.value.trim()) {
+        todoStore.addTodo(text.value)
+        text.value = ''
+    }
+}
+
+const toggleTodo = (index) => {
+    todoStore.toggleTodo(index)
+}
+
+const removeTodo = (index) => {
+    todoStore.removeTodo(index)
+}
+
+const todos = todoStore.todos
+
+</script>
+
+<style></style>
